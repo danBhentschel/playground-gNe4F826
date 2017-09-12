37e1d5a80eb4b965cc1b8071b4e2dba6bd546f82
Dan Hentschel <dan@hentschels.com>
2017-09-12 14:53:33 -0400

    VM files renamed in Alpine docker

--- work/start_vm.sh
+++ work/start_vm.sh
@@ -5,9 +5,9 @@ echo Preparing to start VM...
 echo
 echo
 
-ln -s /vmlinuz-4.4.0-89-generic
-ln -s /initrd.img-4.4.0-89-generic
-ln -s /ubuntu.img
+ln -s /vmlinuz
+ln -s /initramfs
+ln -s /os.img
 ln -s lesson${1}.img scripts.img
 if [ -e /project/target/prepare_vm.sh ]; then
     /project/target/prepare_vm.sh ${1}
